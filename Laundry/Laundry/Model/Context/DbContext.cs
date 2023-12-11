53e5dadb84ee591ea1471ec274021735c8a828df
Agus Heryanto <agusheryantodev@gmail.com>
2023-12-11 22:05:51 +0700

    update : database helper

--- Laundry/Laundry/Model/Context/DbContext.cs
+++ Laundry/Laundry/Model/Context/DbContext.cs
@@ -7,28 +7,22 @@ namespace Laundry.Model.Context
 {
     public class DbContext : IDisposable
     {
-        // deklarasi private variabel / field
         private SQLiteConnection _conn;
 
-        // deklarasi property Conn (connection), untuk menyimpan objek koneksi
         public SQLiteConnection Conn
         {
             get { return _conn ?? (_conn = GetOpenConnection()); }
         }
 
-        // Method untuk melakukan koneksi ke database
         private SQLiteConnection GetOpenConnection()
         {
-            SQLiteConnection conn = null; // deklarasi objek connection
+            SQLiteConnection conn = null;
 
-            try // penggunaan blok try-catch untuk penanganan error
+            try 
             {
-                // atur ulang lokasi database yang disesuaikan dengan
-                // lokasi database perpustakaan Anda
                 string currentDirectory = AppDomain.CurrentDomain.BaseDirectory;
 
                 string databasePath = Path.Combine(currentDirectory,"Laundry.db");
-
 
                 string dbName = databasePath;
 
@@ -38,7 +32,6 @@ namespace Laundry.Model.Context
                 conn = new SQLiteConnection(connectionString); // buat objek connection
                 conn.Open(); // buka koneksi ke database
             }
-            // jika terjadi error di blok try, akan ditangani langsung oleh blok catch
             catch (Exception ex)
             {
                 System.Diagnostics.Debug.Print("Open Connection Error: {0}", ex.Message);
@@ -47,7 +40,6 @@ namespace Laundry.Model.Context
             return conn;
         }
 
-        // Method ini digunakan untuk menghapus objek koneksi dari memory ketika sudah tidak digunakan
         public void Dispose()
         {
             if (_conn != null)
--- Laundry/Laundry/Model/Helper/DatabaseHelper.cs
+++ Laundry/Laundry/Model/Helper/DatabaseHelper.cs
@@ -15,84 +15,105 @@ namespace Laundry.Model.Helper
         public static void InitializeDatabase()
         {
             string currentDirectory = AppDomain.CurrentDomain.BaseDirectory;
-
-            string databasePath = Path.Combine(currentDirectory,"Laundry.db");
-
+            string databasePath = Path.Combine(currentDirectory, "Laundry.db");
             string connectionString = $"Data Source={databasePath};Version=3;";
 
-            if (!File.Exists(connectionString))
+            using (var connection = new SQLiteConnection(connectionString))
             {
-                SQLiteConnection.CreateFile(databasePath);
+                connection.Open();
 
-                using (var connection = new SQLiteConnection(connectionString))
+                // Check if tables exist
+                bool tablesExist = CheckIfTablesExist(connection);
+
+                if (!tablesExist)
                 {
-                    connection.Open();  
-
-                    // membuat tabel
-                    string createEmployeesTableQuery = @"
-                    CREATE TABLE IF NOT EXISTS employees (
-                    id VARCHAR(255) PRIMARY KEY,
-                    username VARCHAR(50) NOT NULL,
-                    name VARCHAR(255) NOT NULL,
-                    password VARCHAR(255) NOT NULL,
-                    auth_password VARCHAR(255) 
-                    );";
-
-                    string createCustomersTableQuery = @"
-                    CREATE TABLE IF NOT EXISTS customers (
-                    id VARCHAR(255) PRIMARY KEY,
-                    name VARCHAR(255),
-                    address VARCHAR(255),
-                    phone_number VARCHAR(255)
-                    );";
-
-                    string createServicesTableQuery = @"
-                    CREATE TABLE IF NOT EXISTS services (
-                    id VARCHAR(255) PRIMARY KEY,
-                    name VARCHAR(255),
-                    price FLOAT,
-                    duration VARCHAR(255)
-                    );";
-
-                    string createTransactionsTableQuery = @"
-                    CREATE TABLE IF NOT EXISTS transactions (
-                    id VARCHAR(255) PRIMARY KEY,
-                    employee_id VARCHAR(255),
-                    customer_id VARCHAR(255),
-                    service_id VARCHAR(255),
-                    weight INT,
-                    status VARCHAR(255),
-                    total DECIMAL(10, 2),
-                    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                    finished_at TIMESTAMP,
-                    FOREIGN KEY (employee_id) REFERENCES employees(id),
-                    FOREIGN KEY (customer_id) REFERENCES customers(id),
-                    FOREIGN KEY (service_id) REFERENCES services(id)
-                    );";
-
-         
-
-                    using (var command = new SQLiteCommand(connection))
-                    {
-                        command.CommandText = createEmployeesTableQuery;
-                        command.ExecuteNonQuery();
-                        command.CommandText = createCustomersTableQuery;
-                        command.ExecuteNonQuery();
-                        command.CommandText = createServicesTableQuery;
-                        command.ExecuteNonQuery();
-                        command.CommandText = createTransactionsTableQuery;
-                        command.ExecuteNonQuery();
-                        connection.Close();
-
-                    }
-
+                    CreateTables(connection);
                 }
+
+                connection.Close();
             }
-            
 
             InsertAdminUserIfNotExist();
-
         }
+
+        private static bool CheckIfTablesExist(SQLiteConnection connection)
+        {
+            string checkTablesQuery = "SELECT name FROM sqlite_master WHERE type='table';";
+            using (var checkTablesCommand = new SQLiteCommand(checkTablesQuery, connection))
+            {
+                using (var reader = checkTablesCommand.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        string tableName = reader.GetString(0);
+                        if (tableName == "employees" || tableName == "customers" || tableName == "services" || tableName == "transactions")
+                        {
+                            return true; 
+                        }
+                    }
+                }
+            }
+
+            return false; 
+        }
+
+        private static void CreateTables(SQLiteConnection connection)
+        {
+            // Membuat tabel
+            string createEmployeesTableQuery = @"
+        CREATE TABLE IF NOT EXISTS employees (
+        id VARCHAR(255) PRIMARY KEY,
+        username VARCHAR(50) NOT NULL,
+        name VARCHAR(255) NOT NULL,
+        password VARCHAR(255) NOT NULL,
+        auth_password VARCHAR(255) 
+        );";
+
+            string createCustomersTableQuery = @"
+        CREATE TABLE IF NOT EXISTS customers (
+        id VARCHAR(255) PRIMARY KEY,
+        name VARCHAR(255),
+        address VARCHAR(255),
+        phone_number VARCHAR(255)
+        );";
+
+            string createServicesTableQuery = @"
+        CREATE TABLE IF NOT EXISTS services (
+        id VARCHAR(255) PRIMARY KEY,
+        name VARCHAR(255),
+        price FLOAT,
+        duration VARCHAR(255)
+        );";
+
+            string createTransactionsTableQuery = @"
+        CREATE TABLE IF NOT EXISTS transactions (
+        id VARCHAR(255) PRIMARY KEY,
+        employee_id VARCHAR(255),
+        customer_id VARCHAR(255),
+        service_id VARCHAR(255),
+        weight INT,
+        status VARCHAR(255),
+        total DECIMAL(10, 2),
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        finished_at TIMESTAMP,
+        FOREIGN KEY (employee_id) REFERENCES employees(id),
+        FOREIGN KEY (customer_id) REFERENCES customers(id),
+        FOREIGN KEY (service_id) REFERENCES services(id)
+        );";
+
+            using (var command = new SQLiteCommand(connection))
+            {
+                command.CommandText = createEmployeesTableQuery;
+                command.ExecuteNonQuery();
+                command.CommandText = createCustomersTableQuery;
+                command.ExecuteNonQuery();
+                command.CommandText = createServicesTableQuery;
+                command.ExecuteNonQuery();
+                command.CommandText = createTransactionsTableQuery;
+                command.ExecuteNonQuery();
+            }
+        }
+
 
 
 
